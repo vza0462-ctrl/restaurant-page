b3646f5fa89d520ae2ea5e91b4f2d97c7cdf1354
vza <vza0462@gmail.com>
2026-04-08 14:06:54 +0700

    build

--- webpack.common.js
+++ webpack.common.js
@@ -1,18 +1,13 @@
-// webpack.config.js
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
+
 module.exports = {
-  mode: "development",
   entry: "./src/index.js",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
-  },
-  devtool: "eval-source-map",
-  devServer: {
-    watchFiles: ["./src/template.html"],
   },
   plugins:[
     new HtmlWebpackPlugin({
